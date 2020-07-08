8b1ce946c031d90285e239990cc9e5f901bba7be
davilbs <davi.ativa@gmail.com>
2020-07-08 10:08:00 -0300

    fix for heroku

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.post("/forecast", function(req, res){
     res.redirect("/");
 });
 
-app.listen(3000, function(){
-    console.log("Listening on port 3000.");
+app.listen(process.env.PORT, function(){
+    console.log("Server is running.");
 });
