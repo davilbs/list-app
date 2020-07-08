439b02d96d4e94cff79c0f047ee4c501b6610817
davilbs <davi.ativa@gmail.com>
2020-07-08 10:02:07 -0300

    update visuals

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
@@ -11,29 +12,45 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 let todoList = ["nothing", "Hous"];
 
-app.get("/", function(req, res){
+app.get("/", function(req, res){    
     let date = new Date();
-
+    
     let options = {
         weekday: "long",
         day:     "numeric",
         month:   "long"
     }
-
+    
     let today = date.toLocaleDateString("pt-BR", options);
     
-    console.log("Got /");
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=Belo Horizonte&appid=6d6655785eff82bcb5b9dc7d71df6229&units=metric";
     
-    res.render("list", {listElement: todoList, day: today});
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            console.log(weatherData.weather[0].icon)
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+            res.render("list", {listElement: todoList, day: today, weather: weatherData, icon: imageURL});
+        });
+    });
+    
 });
 
 app.post("/", function(req, res){
     let newItem = req.body.newItem;
-
-    todoList.push(newItem);
+    if(newItem != ""){
+        todoList.push(newItem);
+    }
 
     res.redirect("/");
-})
+});
+
+app.post("/forecast", function(req, res){
+    res.redirect("/");
+});
 
 app.listen(3000, function(){
     console.log("Listening on port 3000.");
